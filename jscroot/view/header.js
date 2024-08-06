151ea295b1a32e46b1a7c9b46626997d85dd5eff
Rolly Maulana Awangga <awangga@gmail.com>
2024-08-07 03:54:40 +0700

    linkeddevice cek header ga ke pesan lagi

--- jscroot/view/header.js
+++ jscroot/view/header.js
@@ -13,34 +13,19 @@ export function main() {
 
 function getUserFunction(result) {
   if (result.status !== 404) {
-    const { name, phonenumber } = result.data;
-    postJSON(
-      backend.wa.text,
-      "token",
-      getCookie("login"),
-      {
-        to: phonenumber,
-        isgroup: false,
-        messages: "kakak telah melakukan login ke dashboard operator pd.my.id saat ini"
-      },
-      postWAFunction
-    );
+    const { name, linkeddevice } = result.data;
     setInner("headerlogoname", name);
     // Simpan ke localStorage
     localStorage.setItem("nama", name);
+    if (linkeddevice) {
+      setInner("userPoint", "✅");
+      localStorage.setItem("status", "✅");
+    }else{
+      setInner("userPoint", "❌");
+      localStorage.setItem("status", "❌");
+    }
   } 
   else {
     redirect("/login");
-  }
-}
-
-function postWAFunction(result){
-  console.log(result);
-  if (result.status === 200) {
-    setInner("userPoint", "✅");
-    localStorage.setItem("status", "✅");
-  }else{
-    setInner("userPoint", "❌");
-    localStorage.setItem("status", "❌");
   }
 }
