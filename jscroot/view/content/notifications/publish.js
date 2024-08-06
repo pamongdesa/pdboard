0db9e70fb5cf483e59d833c6b8afc869db3f5787
Rolly Maulana Awangga <awangga@gmail.com>
2024-08-07 06:22:49 +0700

    kirim pesan personal tampilan

--- jscroot/view/content/notifications/publish.js
+++ jscroot/view/content/notifications/publish.js
@@ -8,41 +8,19 @@ import { id, backend } from "/dashboard/jscroot/url/config.js";
 
 export async function main(){
     await addCSSIn("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css",id.content);
-    getJSON(backend.project.anggota,'login',getCookie('login'),getResponseFunction);
     onClick("tombolpublishtask",actionfunctionname);
 }
 
-function getResponseFunction(result){
-    console.log(result);
-    if (result.status===200){
-        result.data.forEach(project => {
-            const option = document.createElement('option');
-            option.value = project._id;
-            option.textContent = project.name;
-            document.getElementById('project-name').appendChild(option);
-        });
-
-    }else{
-        Swal.fire({
-            icon: "error",
-            title: result.data.status,
-            text: result.data.response
-          });
-    }
-}
 
 function actionfunctionname(){
     let lap={
-        kode:getValue("project-name"),
-        solusi:getValue("solusi")
+        to:getValue("phone"),
+        isgroup:false,
+        messages:getValue("solusi")
     };
-    if (getCookie("login")===""){
-        redirect("/signin");
-    }else{
-        postJSON(backend.ux.laporan,"login",getCookie("login"),lap,responseFunction);
-        hide("tombolpublishtask");
-        setValue("solusi","");
-    }  
+    postJSON(backend.wa.text,"token",getCookie("login"),lap,responseFunction);
+    hide("tombolprogramtask");
+    setValue("solusi","");
 }
 
 function responseFunction(result){
@@ -51,21 +29,18 @@ function responseFunction(result){
         Swal.fire({
             icon: "success",
             title: "Berhasil",
-            text: "Selamat kak tugas dari "+result.data.nama+" sudah tersimpan dengan kode: "+result.data.kode,
-            footer: '<a href="https://wa.me/'+result.data.phone+'?text='+katakata+'" target="_blank">Verifikasi Proyek</a>',
+            text: "Pesan telah dikirim pada "+result.data.Timestamp+" sudah tersimpan dengan kode: "+result.data.ID,
             didClose: () => {
-                setValue("kode","");
-                setValue("nama","");
                 setValue("phone","");
                 setValue("solusi","");
-                show("tombolpublishtask");
+                show("tombolprogramtask");
             }
           });
     }else{
         Swal.fire({
             icon: "error",
-            title: result.data.status,
-            text: result.data.response
+            title: "Gagal Mengirim Pesan",
+            text: result.data.error
           });
           show("tombolpublishtask");
     }
