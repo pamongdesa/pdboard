03d2ed4bfbde3309199d18dfd97951119aa72ec5
Rolly Maulana Awangga <awangga@gmail.com>
2024-08-06 16:45:25 +0700

    naik lagi satu level

--- jscroot/view/content/profile/accounts.js
+++ jscroot/view/content/profile/accounts.js
@@ -4,7 +4,7 @@ import {getCookie} from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.
 import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 import {addCSSIn} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
 import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/src/sweetalert2.js';
-import { id, backend } from "../../url/config.js";
+import { id, backend } from "../../../url/config.js";
 
 export async function main(){
     await addCSSIn("https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.css",id.content);
