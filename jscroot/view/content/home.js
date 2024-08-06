e68212707bc69e54b7f73f6c517083e35059fc49
Rolly Maulana Awangga <awangga@gmail.com>
2024-08-07 05:37:33 +0700

    hapus yang tidak perlu

--- jscroot/view/content/home.js
+++ jscroot/view/content/home.js
@@ -1,15 +1,6 @@
-import {
-  addCSSIn,
-  setValue,
-  setInner,
-  addChild,
-} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.8/croot.js";
+import {addCSSIn,setInner,addChild} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.8/croot.js";
 import { getCookie } from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
-import {
-  getJSON,
-  putJSON,
-  postJSON,
-} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.8/croot.js";
+import {getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.8/croot.js";
 import { id, backend } from "../../url/config.js";
 
 let tableTemplate = `
@@ -72,8 +63,8 @@ function isiSelesai(result) {
   // Jalankan logika tambahan setelah addChild
   runAfterAddChild(result.data,"done");
   }
-  
 }
+
 function runAfterAddChild(value,idtr) {
   // Temukan elemen tr yang baru saja ditambahkan
   const rows = document.getElementById(idtr).getElementsByTagName("tr");
@@ -85,87 +76,5 @@ function runAfterAddChild(value,idtr) {
     button.addEventListener("click", () => {
       window.open("https://wa.me/"+value.phone, '_blank');
     });
-  }
-}
-
-function putTaskFunction(result) {
-  if (result.status === 200) {
-    getJSON(
-      backend.user.todo,
-      "login",
-      getCookie("login"),
-      getUserTaskFunction
-    );
-    getJSON(
-      backend.user.doing,
-      "login",
-      getCookie("login"),
-      getUserDoingFunction
-    );
-  }
-}
-
-function getUserDoingFunction(result) {
-  setInner("doing", "");
-  setInner("bigdoing", "0");
-  if (result.status === 200) {
-    setInner("bigdoing", "OTW");
-    let content = tableTemplate
-      .replace("#TASKNAME#", result.data.task)
-      .replace("#TASKID#", result.data._id)
-      .replace("#LABEL#", "Beres");
-    addChild("doing", "tr", "", content);
-    // Jalankan logika tambahan setelah addChild
-    runAfterAddChildDoing(result.data);
-  }
-}
-
-function runAfterAddChildDoing(value) {
-  // Temukan elemen tr yang baru saja ditambahkan
-  const rows = document.getElementById("doing").getElementsByTagName("tr");
-  const lastRow = rows[rows.length - 1];
-
-  // Contoh: Tambahkan event listener atau manipulasi DOM lainnya
-  const button = lastRow.querySelector(".button");
-  if (button) {
-    button.addEventListener("click", () => {
-      postJSON(
-        backend.user.done,
-        "login",
-        getCookie("login"),
-        { _id: value._id },
-        postTaskFunction
-      );
-    });
-  }
-}
-
-function postTaskFunction(result) {
-  if (result.status === 200) {
-    getJSON(
-      backend.user.done,
-      "login",
-      getCookie("login"),
-      getUserDoneFunction
-    );
-    getJSON(
-      backend.user.doing,
-      "login",
-      getCookie("login"),
-      getUserDoingFunction
-    );
-  }
-}
-
-function getUserDoneFunction(result) {
-  setInner("done", "");
-  setInner("bigdone", "0");
-  if (result.status === 200) {
-    setInner("bigdone", "OK");
-    let content = tableTemplate
-      .replace("#TASKNAME#", result.data.task)
-      .replace("#TASKID#", result.data._id)
-      .replace("#LABEL#", "Arsip");
-    addChild("done", "tr", "", content);
   }
 }
