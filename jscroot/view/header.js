f9214e678a6082a9a777ebea6d8ac0e780b3129e
Rolly Maulana Awangga <awangga@gmail.com>
2024-08-07 02:14:12 +0700

    postJSON

--- jscroot/view/header.js
+++ jscroot/view/header.js
@@ -2,7 +2,7 @@
 import { getCookie } from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import { redirect } from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 import { setInner } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
-import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
+import { getJSON,postJSON } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
 
 //internal call
 import { backend } from "../url/config.js";
