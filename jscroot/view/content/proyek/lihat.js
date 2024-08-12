449f01e3f2df3a88f44c7a1eaa76e970cc784b48
Rolly Maulana Awangga <awangga@gmail.com>
2024-08-13 06:19:47 +0700

    udah ada putjson hapus redeclaret

--- jscroot/view/content/proyek/lihat.js
+++ jscroot/view/content/proyek/lihat.js
@@ -4,7 +4,6 @@ import {
 } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.7/croot.js";
 import { postJSON,putJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/api.js";
 import { deleteJSON } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.8/croot.js";
-import { putJSON } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.8/croot.js";
 import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
 import { getCookie } from "https://cdn.jsdelivr.net/gh/jscroot/cookie@0.0.1/croot.js";
 import { addCSSIn } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
