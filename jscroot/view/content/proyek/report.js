9dc88e405b852446383ef4a6ed581c868f5def6d
Rolly Maulana Awangga <awangga@gmail.com>
2024-08-12 15:22:50 +0700

    import css in

--- jscroot/view/content/proyek/report.js
+++ jscroot/view/content/proyek/report.js
@@ -1,3 +1,6 @@
+import { addCSSIn } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croot.js";
+
+
 export async function main() {
     await addCSSIn("assets/css/report.css", id.content);
   
